b3ec0ff4ca96ac3cfd8fbdb41b7201f209feb2bd
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2024-01-01 20:28:04 +0700

    finished object trans

--- basics/object-transformation/src/script.js
+++ basics/object-transformation/src/script.js
@@ -9,10 +9,34 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+const group = new THREE.Group();
+scene.add(group);
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 0.5;
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'green' })
+)
+
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'blue' })
+)
+
+cube3.position.x = 2;
+group.add(cube3);
 
 /**
  * Sizes
@@ -28,6 +52,18 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+/**
+ * Axis Helper
+ */
+
+ const axesHelper = new THREE.AxesHelper(5);
+ scene.add(axesHelper);
+
+
+
+
+// Rotation
 
 /**
  * Renderer
