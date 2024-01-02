e77b44f752e03738357266bbf5e856367fc94bdf
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2024-01-03 01:21:18 +0700

    finished section

--- basics/screen-and-resize/src/script.js
+++ basics/screen-and-resize/src/script.js
@@ -22,9 +22,46 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', () =>{
+    // console.log("resize")
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+})
+
+window.addEventListener('dblclick', () =>{
+    // console.log("dblclick");
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
+
+    if(!fullscreenElement){
+        if(canvas.requestFullscreen){
+            canvas.requestFullscreen();
+        }
+        else if(canvas.webkitRequestFullscreen){
+            canvas.webkitRequestFullscreen();
+        }
+    }
+    else{
+        if(document.exitFullscreen){
+            document.exitFullscreen();
+        }
+        else if(document.webkitExitFullscreen){
+            document.webkitExitFullscreen();
+        }
+        console.log("leave fullscreen")
+    }
+})
 
 /**
  * Camera
@@ -44,8 +81,8 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-renderer.setSize(sizes.width, sizes.height)
-
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 /**
  * Animate
  */
